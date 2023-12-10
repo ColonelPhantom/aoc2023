061517d33ba7599021f65774a73173bf9dfdcbf9
Quinten Kock <quintenkock@gmail.com>
2023-12-10 20:38:50 +0100

    day 10 part 2

--- Day10.hs
+++ Day10.hs
@@ -1,7 +1,10 @@
 module Day10 where
 
 import qualified Data.Map as M
+import qualified Data.Set as S
 import Data.Maybe (catMaybes, mapMaybe, fromMaybe)
+import Debug.Trace (traceShowId, traceShow)
+import Data.List (sort)
 
 type Neighs a = a -> [a]
 
@@ -25,7 +28,10 @@ inpToMap xs = M.fromList elems where
     entry x y char = if char /= '.' then Just ((x,y), char) else Nothing
 
 neighs :: M.Map Coord Char -> Coord -> [Coord]
-neighs m c@(x,y) = case m M.! c of
+neighs m c@(x,y) = neighs' m c (m M.! c)
+
+neighs' :: M.Map Coord Char -> Coord -> Char -> [Coord]
+neighs' m c@(x,y) char = case char of
                   '.' -> error $ "expected pipe, found ground at " ++ show c
                   'S' -> filter validS [(x-1,y), (x+1,y), (x,y-1), (x,y+1)]
                   '|' -> [(x, y-1), (x, y+1)]
@@ -43,6 +49,38 @@ printMap m = unlines $ map line [0..maxY] where
     maxX = maximum $ map (fst . fst) (M.toList m)
     maxY = maximum $ map (snd . fst) (M.toList m)
 
+-- breathe :: M.Map Coord Char -> M.Map Coord Char
+breathe m = M.unions $ map (uncurry expand) $ M.toList m where
+    base :: Coord -> Char -> M.Map Coord Char
+    base (x',y') c = let (x,y) = (3*x'+1, 3*y'+1) in M.fromList [((x,y), c)]
+
+    expand :: Coord -> Char -> M.Map Coord Char
+    expand (x,y) '|' = M.union (M.fromList [((3*x+1, 3*y+0), '|'), ((3*x+1, 3*y+2), '|')]) (base (x,y) '|')
+    expand (x,y) '-' = M.union (M.fromList [((3*x+0, 3*y+1), '-'), ((3*x+2, 3*y+1), '-')]) (base (x,y) '-')
+    expand (x,y) 'L' = M.union (M.fromList [((3*x+1, 3*y+0), '|'), ((3*x+2, 3*y+1), '-')]) (base (x,y) 'L')
+    expand (x,y) 'F' = M.union (M.fromList [((3*x+1, 3*y+2), '|'), ((3*x+2, 3*y+1), '-')]) (base (x,y) 'F')
+    expand (x,y) 'J' = M.union (M.fromList [((3*x+1, 3*y+0), '|'), ((3*x+0, 3*y+1), '-')]) (base (x,y) 'J')
+    expand (x,y) '7' = M.union (M.fromList [((3*x+1, 3*y+2), '|'), ((3*x+0, 3*y+1), '-')]) (base (x,y) '7')
+    expand (x,y) 'S' = expand (x,y) (realS (x,y))
+
+    realS :: Coord -> Char
+    realS c = let s = sort $ neighs' m c 'S' in head $ filter (\r -> sort (neighs' m c r) == s) "|-LJ7F"
+
+
+
+outsideNeighs :: M.Map Coord Char -> Coord -> [Coord]
+outsideNeighs m (x,y) = {-let r = -} filter notPipe [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]{- in traceShow ("neighs", (x,y), r) r-} where
+    notPipe c@(x,y) = x >= 0 && x <= maxX && y >= 0 && y <= maxY && c `M.notMember` m -- we pass in the expanded . bfs'ed pipe, which only contains the expanded pipe and no ground or other elements
+    maxX = 3 + maximum (map (fst . fst) (M.toList m))
+    maxY = 3 + maximum (map (snd . fst) (M.toList m))
+
+inside :: M.Map Coord Char -> Int
+inside m = sum $ map line [0..maxY] where
+    line y = length $ filter (\x -> M.notMember (x,y) m) [0..maxX]
+    maxX = maximum $ map (fst . fst) (M.toList m)
+    maxY = maximum $ map (snd . fst) (M.toList m)
+
+
 main :: IO ()
 main = do
     input <- lines <$> getContents
@@ -53,6 +91,16 @@ main = do
     putStr "part 1: "; print $ maximum $ M.elems search_result
 
     let loop_only = M.mapWithKey (\c d -> m M.! c) search_result
-    putStrLn $ printMap loop_only
 
-    putStrLn "part 2: "
+    let b = breathe loop_only
+    putStr $ printMap m
+    putStr $ printMap b
+
+    let reachable' = M.keys $ bfs (outsideNeighs b) [((0,0), 0)] M.empty
+    let reachable = map (\(x,y) -> (x `div` 3, y `div` 3)) $ filter (\(x,y) -> x `mod` 3 == 1 && y `mod` 3 == 1) reachable'
+    print reachable
+
+    let io = M.union (M.fromList $ map (, 'O') reachable) loop_only
+
+--     putStr $ printMap io
+    putStr "part 2: "; print $ inside io
