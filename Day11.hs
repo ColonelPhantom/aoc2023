23e84e63afd5906ca23b77f83da13a5f4cf7d245
Quinten Kock <quinten@quinten.space>
2023-12-11 07:21:59 +0100

    Speed up day11 with smallint and Data.Array

--- Day11.hs
+++ Day11.hs
@@ -2,6 +2,7 @@ module Day11 where
 import Data.List
 import Data.Maybe
 import qualified Data.Map as M
+import qualified Data.Array as A
 import Debug.Trace (traceShow)
 
 empty :: [Char] -> Bool
@@ -15,20 +16,20 @@ expand = concatMap go where
 expand' = transpose . expand . transpose
 
 -- find all galaxies
-galaxies :: [[Char]] -> [(Integer, Integer)]
+galaxies :: [[Char]] -> [(Int, Int)]
 galaxies xs = elems where
     elems = catMaybes $ concat $ zipWith linep [0..] xs
     linep y = zipWith (`entry` y) [0..]
     entry x y char = if char /= '.' then Just (x,y) else Nothing
 
-manhattan :: (Integer, Integer) -> (Integer, Integer) -> Integer
+manhattan :: (Int, Int) -> (Int, Int) -> Int
 manhattan (a,b) (x,y) = abs (x-a) + abs (y-b)
 
 -- part 2:
 -- use prefix sum to get 'real' coordinates based on map sizes
-expand2 :: Integer -> [[Char]] -> M.Map Integer Integer
-expand2 step xs = M.fromList $ zip [0..] $ scanl1 (+) $ map go xs where
-    go :: [Char] -> Integer
+expand2 :: Int -> [[Char]] -> A.Array Int Int
+expand2 step xs = A.listArray (0, length xs) $  scanl1 (+) $ map go xs where
+    go :: [Char] -> Int
     go line = if empty line then 1 else step
 
 main :: IO ()
@@ -47,7 +48,7 @@ main = do
     let ys = expand2 step input
     let xs = expand2 step (transpose input)
     let g = galaxies input
-    let mapCoord (x,y) = (xs M.! x, ys M.! y)
+    let mapCoord (x,y) = (xs A.! x, ys A.! y)
     let dists = [manhattan (mapCoord a) (mapCoord b) | (i, a) <- zip [1..] g, (j,b) <- zip [1..] g, i < j]
 
     print $ sum dists
